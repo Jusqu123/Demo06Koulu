16c59560700c38129480179bf7d879d807f98d90
Jusqu123 <jusquu123@gmail.com>
2016-01-27 14:32:25 +0200

    tein tämän koulussa

---
+++ Demo06Projekti/Persons.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06Projekti
+{
+    /// <summary>
+    /// This class holds persons objects and give a few methods...
+    /// </summary>
+    class Persons
+    {
+        /// <summary>
+        /// Persons collection stores person objects.
+        /// </summary>
+        private List<Person> persons;
+
+        /// <summary>
+        /// Default constructor initializes a collection
+        /// </summary>
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+        /// <summary>
+        /// adds a person to collection
+        /// </summary>
+        /// <param name="person">added person</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+        /// <summary>
+        /// Returns a person to caller application
+        /// </summary>
+        /// <param name="index">Person index in collection</param>
+        /// <returns>returned person object</returns>
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count) return persons.ElementAt(index);
+            else return null;
+        }
+        public Person FindPerson(string socialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                    //person with sotu found....
+                { return person; }
+            }//person with sotu not found...
+            return null;
+        }
+        public void PrintCollection()
+        {
+            Console.WriteLine("Persons in collection:");
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
---
+++ Demo06Projekti/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo06Projekti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an object from persons
+            //myfriends voi olla ihan mikä vaan
+            Persons myFriends = new Persons();
+            // create a frieands...
+            Person person1 = new Person {Firstname ="Juuso", Lastname="hattunen",SocialSecurityNumber="010101-234H" };
+            Person person2 = new Person { Firstname = "veli", Lastname = "viljami", SocialSecurityNumber = "675935-976B" };
+            Person person3 = new Person { Firstname = "Matias", Lastname = "jokinen", SocialSecurityNumber = "836504-231P" };
+            //add persons to collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+
+            //print collection
+            myFriends.PrintCollection();
+
+
+            //get one person
+            Console.WriteLine("get on person from collection: ");
+         Person person4 =  myFriends.GetPerson(0);
+            if(person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Cannot find person in that position...");
+            }
+            //find person with sotu
+
+            string socialSecurityNumber = "01001-234H";
+            Console.WriteLine("find person with sotu :" + socialSecurityNumber);
+           Person person5 = myFriends.FindPerson(socialSecurityNumber);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("not found...");
+            }
+        }
+    }
+}
+
+
+
+//https://github.com/Jusqu123/Demo06Koulu.git
